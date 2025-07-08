96ffdc61c82f65f48d317c491bef28f9a01b35c9
nicole.soto0864@gmail.com <nicole.soto0864@gmail.com>
2025-07-08 15:31:19 +0200

    add: package

--- formatter/formatter.go
+++ formatter/formatter.go
@@ -3,6 +3,7 @@ package formatter
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 )
 
 func PrettyPrint(data []byte) ([]byte, error) {
@@ -15,4 +16,8 @@ func Minify(data []byte) ([]byte, error) {
 	var out bytes.Buffer
 	err := json.Compact(&out, data)
 	return out.Bytes(), err
+}
+
+func hello() {
+	fmt.Println("Hello! :)")
 }
--- main.go
+++ main.go
@@ -9,6 +9,8 @@ type Options struct {
 	file     string
 	validate bool
 	minify   bool
+	prettify bool
+	output   bool
 }
 
 func main() {
@@ -17,9 +19,16 @@ func main() {
 	flag.StringVar(&options.file, "file", "", "Path to json file")
 	flag.BoolVar(&options.minify, "minify", false, "Minify json output")
 	flag.BoolVar(&options.validate, "validate", false, "Validate json input")
+	flag.BoolVar(&options.output, "output", false, "Output json to stdout")
+	flag.BoolVar(&options.prettify, "prettify", false, "Prettify json output")
 
 	flag.Parse()
 	input := flag.Arg(0)
 
 	fmt.Println("Flags parsed:", options.file, input)
+	formatter.hello()
 }
+
+//process file
+// create new file
+// put converted into the file
